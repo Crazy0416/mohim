b672a5b92ff1ce900ea3cb2cb8b531b2ede88a9c
kimminho <minmin0416@naver.com>
2018-12-07 13:20:37 +0900

    App: url 로그 추가.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+'use strict';
+
+require('./app/helpers/logHandler');
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-require('./app/helpers/logHandler');
 const errorHandle = require('./app/helpers/errors/errorHandle');
 
 const indexRouter = require('./app/routes/index');
@@ -14,6 +16,14 @@ const app = express();
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'ejs');
 
+// url logger
+app.use((req, res, next) => {
+	res.on('finish', () => {
+		logger.info(`${req.method} ${res.statusCode} ${req.originalUrl} -- ${res.statusMessage}; ${res.get('Content-Length') || 0}b sent`)
+	});
+	next();
+});
+
 // app use setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,14 +32,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// url logger
-app.use((req, res, next) => {
-	res.on('finish', () => {
-		logger.info(`${req.method} ${res.statusCode} ${req.originalUrl} -- ${res.statusMessage}; ${res.get('Content-Length') || 0}b sent`)
-	});
-	next();
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
