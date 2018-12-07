3161b920801f95c9e8821a378da933956034a114
kimminho <minmin0416@naver.com>
2018-12-07 13:21:22 +0900

    Config: mysql 설정값 추가.

--- config/config.js
+++ config/config.js
@@ -4,30 +4,38 @@
 	[
 		'NODE_ENV'
 	].forEach((name) => {
-	if (!process.env[name]) {
-		throw new Error(`Environment variable ${name} is missing`)
-	}
-});
+		if (!process.env[name]) {
+			throw new Error(`Environment variable ${name} is missing`)
+		}
+	});
 const config = {};
 
 if(process.env.NODE_ENV === "dev") {
-	config.mongodb = {
+	config.mysql = {
+		"CONNECTION_POOL": 10,
 		"DATABASE": 'mohim',
-		"PORT": '27017',
-		"HOST": 'localhost'
+		"USER": 'root',
+		"PASSWORD": 'KIMho0715!@',
+		"HOST": 'localhost',
+		"PORT": 3306
 	};
 	config.server = {
 		PORT: 3000
-	}
+	};
+	config.logLevel = 'debug'
 } else if(process.env.NODE_ENV === "prod") {
-	config.mongodb = {
+	config.mysql = {
+		"CONNECTION_POOL": 10,
 		"DATABASE": 'mohim',
-		"PORT": '27017',
-		"HOST": 'localhost'
+		"USER": 'root',
+		"PASSWORD": 'KIMho0715!@',
+		"HOST": 'localhost',
+		"PORT": 3306
 	};
 	config.server = {
 		PORT: 5900
-	}
+	};
+	config.logLevel = 'info'
 }
 
 module.exports = config;
